f82160caa3a08af83ea89a6da6403a1166b9415e
Tom Stuart <tom@experthuman.com>
2011-11-03 16:37:11 +0000

    Generate fresh Rails 3.1.1 app
    rails new contact-o-tron --database mysql --skip-sprockets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactOTron::Application.config.secret_token = '259ada433dd6e2b9387ec19d467a573d91fe1089e99006a8911489aaece96ed181fe0e051ee3618ead2492a04e4634ed22ae42a391c2a28ad05e159c65716a35'
