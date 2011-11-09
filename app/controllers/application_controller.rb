3d8d8bd5f66acc37cb00952da6d783899f21941f
James Weiner <jw@alphagov.co.uk>
2011-11-09 14:22:15 +0000

    Telling slimmer we want to use the admin template and stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_slimmer_header
+  
+  def set_slimmer_header
+    headers['X-Slimmer-Template'] = 'admin'
+  end
 end
