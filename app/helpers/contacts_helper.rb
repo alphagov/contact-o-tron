b5639439ab72856c7d497d7a2a3ffb2f43fcc4ac
Tom Stuart <tom@experthuman.com>
2011-11-16 09:59:18 +0000

    Always explicitly show an extra blank phone number form

--- app/helpers/contacts_helper.rb
+++
@@ -1,6 +0,0 @@
-module ContactsHelper
-  def phone_numbers_for(contact)
-    new_phone_numbers, existing_phone_numbers = contact.phone_numbers.partition(&:new_record?)
-    existing_phone_numbers + (new_phone_numbers.presence || [PhoneNumber.new])
-  end
-end
