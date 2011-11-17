5456fc1f0797d122e6aba188f983bf24fdb6a31c
Tom Stuart <tom@experthuman.com>
2011-11-17 14:43:51 +0000

    Connect Stomp client after Passenger worker forks

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -4,5 +4,14 @@ unless Rails.env.test?
   host = Rails.env.production? ? 'support.cluster' : 'localhost'
   uri = URI::Generic.build scheme: 'stomp', host: host, port: 61613
   failover_uri = "failover://(#{Array.new(2, uri).join(',')})" # failover://() must have multiple URIs
-  Messenger.transport = Stomp::Client.new failover_uri
+
+  if defined?(PhusionPassenger)
+    PhusionPassenger.on_event(:starting_worker_process) do |forked|
+      if forked
+        Messenger.transport = Stomp::Client.new failover_uri
+      end
+    end
+  else
+    Messenger.transport = Stomp::Client.new failover_uri
+  end
 end
