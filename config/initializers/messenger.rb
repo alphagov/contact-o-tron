c573128540e5613dedae6e412e61d13fef5ebd6f
Tom Stuart <tom@experthuman.com>
2011-11-17 14:39:25 +0000

    Use a failover:// URI for Stomp::Client to allow reconnection

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -3,5 +3,6 @@ require 'messenger'
 unless Rails.env.test?
   host = Rails.env.production? ? 'support.cluster' : 'localhost'
   uri = URI::Generic.build scheme: 'stomp', host: host, port: 61613
-  Messenger.transport = Stomp::Client.new uri.to_s
+  failover_uri = "failover://(#{Array.new(2, uri).join(',')})" # failover://() must have multiple URIs
+  Messenger.transport = Stomp::Client.new failover_uri
 end
