fc7c8e6aa30c830ea7662122eaa964c190f9e2d6
Tom Stuart <tom@experthuman.com>
2011-11-17 14:57:28 +0000

    Remove redundant ContactObserver
    This work is now being done automatically by Marples::ModelActionBroadcast.
    
    This reverts commit 3fae8d163efc0764384c62d5b8b8a97a91457643.

--- app/models/contact_observer.rb
+++
@@ -1,9 +0,0 @@
-class ContactObserver < ActiveRecord::Observer
-  def after_create(contact)
-    Messenger.instance.created contact
-  end
-
-  def after_update(contact)
-    Messenger.instance.updated contact
-  end
-end
