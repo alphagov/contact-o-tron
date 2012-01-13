a214b23af42dd4e3f33b715f172e65febf0396df
James Stewart <jys@ketlai.co.uk>
2012-01-13 12:50:08 +0000

    Add missing comma

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 ContactOTron::Application.config.session_store :cookie_store,
-  key: '_contact-o-tron_session'
+  key: '_contact-o-tron_session',
   secure: Rails.env.production?,
   http_only: true
